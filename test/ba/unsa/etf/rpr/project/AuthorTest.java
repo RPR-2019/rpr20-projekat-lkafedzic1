16099c6eddc3d29806582f34a7637a4ddb3841b7
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 12:44:42 +0100

    first tests initialised

---
+++ test/ba/unsa/etf/rpr/project/AuthorTest.java
@@ -0,0 +1,60 @@
+package ba.unsa.etf.rpr.project;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorTest {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    Author author = null;
+
+    @BeforeEach
+    void before() {
+        author = new Author("Ela","Kagocic", simpleDateFormat.format("20.12.1997."), Gender.FEMALE);
+    }
+
+    @Test
+    void getId() {
+        assertEquals(0, author.getId());
+    }
+
+    @Test
+    void setFirstname() {
+        author.setFirstname("Ella");
+        assertEquals("Ella", author.getFirstname());
+    }
+
+    @Test
+    void testEquals() {
+        Author other = new Author("Ela", "Kagocic", simpleDateFormat.format("20.12.1997."), Gender.FEMALE);
+        assertEquals(author, other);
+    }
+
+    @Test
+    void getGender() {
+        assertEquals(Gender.FEMALE, author.getGender());
+    }
+
+    @Test
+    void setGender() {
+    }
+
+    @Test
+    void getDateOfBirth() {
+    }
+
+    @Test
+    void setDateOfBirth() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+}
