ad5b97030bc40171d4287700c8ea28a0c205f31d
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 12:43:27 +0100

    controller initialised

---
+++ src/ba/unsa/etf/rpr/project/SignUpController.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.project;
+
+import javafx.event.ActionEvent;
+
+public class SignUpController {
+
+    public void actionOk(ActionEvent actionEvent) {
+    }
+
+    public void actionCancel(ActionEvent actionEvent) {
+    }
+}
