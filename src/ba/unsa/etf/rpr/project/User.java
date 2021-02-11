b4396d0ed1ac198c45060657f53c4e95b7813f96
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 12:38:58 +0100

    initialised class

---
+++ src/ba/unsa/etf/rpr/project/User.java
@@ -0,0 +1,155 @@
+package ba.unsa.etf.rpr.project;
+
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class User implements Person {
+    private int id = 0;
+    private SimpleStringProperty firstname, lastname;
+    private SimpleStringProperty dateOfBirth;
+    private SimpleObjectProperty<Gender> gender;
+    private SimpleStringProperty username = new SimpleStringProperty();
+    private SimpleStringProperty password = new SimpleStringProperty();
+    private SimpleStringProperty email = new SimpleStringProperty();
+    private String image = "";
+
+    public User(int id, String firstname, String lastname, String dateOfBirth, Gender gender, String username, String password, String email, String image) {
+        this.id = id;
+        this.firstname = new SimpleStringProperty(firstname);
+        this.lastname = new SimpleStringProperty(lastname);
+        this.dateOfBirth = new SimpleStringProperty(dateOfBirth);
+        this.gender = new SimpleObjectProperty<Gender>(gender);
+        this.username = new SimpleStringProperty(username);
+        this.password = new SimpleStringProperty(password);
+        this.email = new SimpleStringProperty(email);
+        this.image = new String();
+    }
+
+    public User() {
+        id = 0;
+        firstname = new SimpleStringProperty();
+        lastname = new SimpleStringProperty();
+        dateOfBirth = new SimpleStringProperty();
+        gender = new SimpleObjectProperty<>();
+        username = new SimpleStringProperty();
+        password = new SimpleStringProperty();
+        email = new SimpleStringProperty();
+        image = new String();
+    }
+
+    public User(String firstname, String lastname, String dateOfBirth, Gender gender) {
+        this.firstname = new SimpleStringProperty(firstname);
+        this.lastname = new SimpleStringProperty(lastname);
+        this.dateOfBirth = new SimpleStringProperty(dateOfBirth);
+        this.gender = new SimpleObjectProperty<>(gender);
+    }
+
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getFirstname() {
+        return firstname.get();
+    }
+
+    public SimpleStringProperty firstnameProperty() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname.set(firstname);
+    }
+
+    @Override
+    public String getLastname() {
+        return lastname.get();
+    }
+
+    public SimpleStringProperty lastnameProperty() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname.set(lastname);
+    }
+
+    @Override
+    public String getDateOfBirth() {
+        return dateOfBirth.get();
+    }
+
+    public SimpleStringProperty dateOfBirthProperty() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth.set(dateOfBirth);
+    }
+
+    @Override
+    public Gender getGender() {
+        return gender.get();
+    }
+
+    public SimpleObjectProperty<Gender> genderProperty() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender.set(gender);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getPassword() {
+        return password.get();
+    }
+
+    public SimpleStringProperty passwordProperty() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password.set(password);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
