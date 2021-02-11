9c2fd30a2e66236a75755fd03a4ce4476272c3ed
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 12:39:20 +0100

    interface class initialised

---
+++ src/ba/unsa/etf/rpr/project/Person.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr.project;
+
+import java.text.SimpleDateFormat;
+
+public interface Person {
+
+    public int getId();
+
+    public void setId(int id);
+
+    public String getFirstname();
+
+    public void setFirstname(String firstname);
+
+    public String getLastname();
+
+    public void setLastname(String lastname);
+
+    public Gender getGender();
+
+    public void setGender(Gender gender);
+
+    public String getDateOfBirth();
+
+    public void setDateOfBirth(String dateOfBirth);
+
+}
