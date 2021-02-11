357e30dd7a22e0248b1f966adaea4b9479992f9d
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 14:44:22 +0100

    click on cancel resolved

--- src/ba/unsa/etf/rpr/project/SignUpController.java
+++ src/ba/unsa/etf/rpr/project/SignUpController.java
@@ -1,12 +1,19 @@
 package ba.unsa.etf.rpr.project;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class SignUpController {
 
+    public Label lblFirstName;
+
     public void actionOk(ActionEvent actionEvent) {
+        //sačuvati
     }
 
     public void actionCancel(ActionEvent actionEvent) {
+        Stage stage = (Stage) lblFirstName.getScene().getWindow();
+        stage.close();
     }
 }
