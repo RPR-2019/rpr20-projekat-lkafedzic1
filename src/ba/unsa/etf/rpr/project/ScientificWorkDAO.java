a4f4c14161fc9773d1acac42cef27b3f820084f7
lkafedzic1 <zivotinje20.12.1997>
2021-02-11 15:10:23 +0100

    DAO initial commit

--- src/ba/unsa/etf/rpr/project/ScientificWorkDAO.java
+++ src/ba/unsa/etf/rpr/project/ScientificWorkDAO.java
@@ -3,4 +3,10 @@ package ba.unsa.etf.rpr.project;
 public class ScientificWorkDAO {
     private static ScientificWorkDAO instance;
 
+    public static ScientificWorkDAO getInstance() {
+        if (instance == null) instance = new ScientificWorkDAO();
+        return instance;
+    }
+    private ScientificWorkDAO() {}
+
 }
