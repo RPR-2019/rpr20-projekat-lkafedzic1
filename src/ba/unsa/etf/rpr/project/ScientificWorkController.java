3cfa814e0b5adb9de39b4c75ab732ac808ade27d
lkafedzic1 <zivotinje20.12.1997>
2021-02-12 13:00:47 +0100

    Minor changes and new form for adding works

--- src/ba/unsa/etf/rpr/project/ScientificWorkController.java
+++ src/ba/unsa/etf/rpr/project/ScientificWorkController.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.project;
 
+import javafx.event.ActionEvent;
+
 public class ScientificWorkController {
+    public void actionAddScienceWork(ActionEvent actionEvent) {
+    }
+
+    public void actionCancel(ActionEvent actionEvent) {
+    }
 }
