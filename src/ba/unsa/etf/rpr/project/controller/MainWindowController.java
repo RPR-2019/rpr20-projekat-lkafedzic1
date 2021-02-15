15f6b37e5e158f99fa8d437ca4dece793d348f4f
lkafedzic1 <zivotinje20.12.1997>
2021-02-15 10:22:13 +0100

    added view for administrator

--- src/ba/unsa/etf/rpr/project/controller/MainWindowController.java
+++ src/ba/unsa/etf/rpr/project/controller/MainWindowController.java
@@ -1,8 +1,18 @@
 package ba.unsa.etf.rpr.project.controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
 public class MainWindowController {
 
     public Label lblStatusBar;
+
+    public void actionSearch(ActionEvent actionEvent) {
+    }
+
+    public void actionSelectFile(ActionEvent actionEvent) {
+    }
+
+    public void actionClose(ActionEvent actionEvent) {
+    }
 }
